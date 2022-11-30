37c926b2306b617fec99f889183ff424fdbe1dd9
connorbottone <42557300+connorbottone@users.noreply.github.com>
2022-11-29 22:52:05 -0500

    began code

---
+++ assets/script.js
@@ -0,0 +1,10 @@
+//maker  a array to store questions and andersd 
+// create a function to start the Game
+
+// start a timer when the game starts
+// wen the first question is answered the next will follow
+// if question is inccorct time will be subtracted from timer
+// the user will be diplayed correct or incorect below the questiions based on there selecton
+// wen the timer reaches 0 or all the questions are answered the game is over 
+// wen game is over log socre wit initals and final score 
+// after score is srtoed user can view high scores or play again
